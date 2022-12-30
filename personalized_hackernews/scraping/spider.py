ad0aa36022249d3d376b2e2cc22480c602988971
Li-Wei Chi <lchi021497@gmail.com>
2022-12-30 12:35:22 -0500

    improve web scraping

--- personalized_hackernews/scraping/spider.py
+++ personalized_hackernews/scraping/spider.py
@@ -2,18 +2,30 @@ import os
 from time import gmtime, strftime
 from urllib.parse import urlparse
 
+from bs4 import BeautifulSoup
 import scrapy
-from items import HNItem, SiteItem
+from scraping.items import HNItem, SiteItem
 from scrapy.loader import ItemLoader
 from scrapy.selector import Selector
+from scraping.scrape import parse_paragraphs
+import logging
+from scrapy.utils.log import configure_logging 
 
 # parse first ten pages of HN
-PAGE_DEPTH_LIMIT = 10
+configs = {
+    'PAGE_DEPTH_LIMIT': 10, # number of pages to crawl on hackernews
+    'UPVOTE_THRESHOLD': 20, # number of upvotes
+    'PGRAPH_LEN_THRES': 30,
+    'PGRAPH_ROLLING_WINDOW': 5,
+    'EXCLUDE_SITE_SUFFIXES': ['.pdf', 'robots.txt'],
+    'LOGFILE_NAME': 'logfile',
+}
+
 
 class HNSpider(scrapy.Spider):
     '''
        Spider that scrapes the hacker news page.
-       Scrapes the title, source_url(to crawl), src domain,
+       Scrapes the title, source_url(to crawl), src domain``
        score, author, age of each page.
        
        Send Requests of the urls crawled to the scheduler
@@ -26,8 +38,15 @@ class HNSpider(scrapy.Spider):
         'https://news.ycombinator.com/',
     ]
 
-    LOG_FILE = "logfile"
+    LOG_FILE = configs['LOGFILE_NAME']
     page_depth = 0
+
+    configure_logging(install_root_handler=False)
+    logging.basicConfig(
+        filename=LOG_FILE,
+        format='%(levelname)s: %(message)s',
+        level=logging.INFO
+    )
 
     def check_list_len(self, list_name, mylist, expected_len):
         if (len(mylist) != expected_len):
@@ -43,7 +62,7 @@ class HNSpider(scrapy.Spider):
         scores = []
         authors = []
         ages = []
-        for td in response.css("td"):
+        for td in entries:
             entry = td.css(".title")
             subtext = td.css(".subtext")
 
@@ -51,11 +70,14 @@ class HNSpider(scrapy.Spider):
             if not title or title == "More":
                 continue
 
-            src_url = entry.css("a.titlelink::attr(href)").get()
+            src_url = entry.css("a::attr(href)").get()
             src = entry.css(".sitebit a span::text").get()
             score = subtext.css(".score::text").get()
             author = subtext.css(".hnuser::text").get()
             age = subtext.css(".age a::text").get()
+
+            # if score is None or int(score.split(' ')[0]) < configs['UPVOTE_THRESHOLD']:
+            #     continue
 
             # defaults
             if score == None:
@@ -70,6 +92,7 @@ class HNSpider(scrapy.Spider):
             authors.append(author)
             ages.append(age)
 
+        # sanity check: check that all fields have same length
         list_len = len(titles)
         self.check_list_len("src_urls", src_urls, list_len)
         self.check_list_len("srcs", srcs, list_len)
@@ -91,10 +114,20 @@ class HNSpider(scrapy.Spider):
             yield l.load_item()
 
         for url in src_urls:
+            print('following url: ', url)
+            if url is None:
+                continue
+            for suffix in configs['EXCLUDE_SITE_SUFFIXES']:
+                if url.endswith(suffix):
+                    continue
+            # TODO: handle hn posts differently
+            if urlparse(url).netloc == 'www.washingtonpost.com':
+                break
+
             yield response.follow(url, self.parse_site)
 
         # parse more on later paginations
-        if self.page_depth < PAGE_DEPTH_LIMIT:
+        if self.page_depth < configs['PAGE_DEPTH_LIMIT']:
             self.page_depth += 1
             next_page_path = response.css(".morelink::attr(href)").get() 
            
@@ -108,8 +141,16 @@ class HNSpider(scrapy.Spider):
         l.add_css("title", "h1::text")
         l.add_css("subtitles", "h2::text")
         l.add_css("subtitles", "h3::text")
-        # l.add_css("paragraphs", "p::text")
-        l.add_xpath("paragraphs", '//div[@class="Post"]')
+ 
+        soup = BeautifulSoup(response.text, 'html.parser')
+        pgraphs = soup.find_all('p')
+
+        # for github sites, parse list as paragraphs
+        domain = urlparse(response.url).netloc
+        list_as_paragraph = (domain.find('github.com') != -1)
+        parsed_paragraphs = parse_paragraphs(pgraphs, configs['PGRAPH_LEN_THRES'], configs['PGRAPH_ROLLING_WINDOW'], list_as_paragraph=list_as_paragraph)
+
+        l.add_value("paragraphs", parsed_paragraphs)
         l.add_value("href", response.url)
         l.add_css("relevantHrefs", "body a::attr(href)")
         l.add_css("date", "time::text")
