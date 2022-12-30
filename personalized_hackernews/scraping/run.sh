933d6d9b135fedde994884ddd5dbf5a0bbb206df
Li-Wei Chi <lchi021497@gmail.com>
2022-12-30 12:39:11 -0500

    add env vars setup

--- personalized_hackernews/scraping/run.sh
+++ personalized_hackernews/scraping/run.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/zsh
 
-source venv/bin/activate
-export POST_DB_NAME="mongo_hnposts_1"
-export SITE_DB_NAME="mongo_sites_1"
+source ../venv/bin/activate
+export POST_DB_NAME="mongo_hnposts_2"
+export SITE_DB_NAME="mongo_sites_2"
 
-scrapy runspider spider.py --logfile=logfile
+export PATH=$PATH:/opt/anaconda3/bin
+# export PROJECT_HOME='' # export project home dir
+export PYTHONPATH=${PROJECT_HOME}/personalized_hackernews
+
+scrapy runspider spider.py
